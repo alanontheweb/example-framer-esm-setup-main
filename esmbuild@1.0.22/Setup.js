f967ebc68bbf0a711a21acf7ecaa5e7b6808e90d
deploy <deploy@users.noreply.github.com>
2022-01-09 08:10:54 +0000

    Updated

---
+++ esmbuild@1.0.22/Setup.js
@@ -0,0 +1,26 @@
+import { setup } from "twind";
+const Setup = () => {
+  setup({
+    theme: {
+      colors: {
+        gray: "red",
+        red: "blue",
+        blue: "green",
+        yellow: "yellow"
+      },
+      fontFamily: {
+        sans: ["Helvetica", "sans-serif"],
+        serif: ["Times", "serif"]
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem"
+        }
+      }
+    }
+  });
+};
+export {
+  Setup
+};
