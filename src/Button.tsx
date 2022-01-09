2c600bbd2d821c54f6ee9d6b749645560e6bbb81
Alan Roy <alanontheweb@users.noreply.github.com>
2022-01-08 18:18:12 -0800

    test

---
+++ src/Button.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { styled } from "@twind/react";
+import { tw, setup } from "twind";
+import { addPropertyControls, ControlType } from "framer";
+
+// import Hello from "./Test.tsx"
+
+// Learn more: https://www.framer.com/docs/guides/code-components/
+
+export const Test2 = () => {
+  setup({
+    theme: {
+      colors: {
+        // Build your palette here
+        gray: "red",
+        red: "yellow",
+        blue: "green",
+        yellow: "yellow",
+      },
+      fontFamily: {
+        sans: ["Helvetica", "sans-serif"],
+        serif: ["Times", "serif"],
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+      },
+    },
+  });
+
+  const Button = styled("button", {
+    base: `
+    appearance-none border-none bg-transparent
+    rounded-full px-2.5
+  `,
+
+    variants: {
+      size: {
+        sm: `text-sm h-6`,
+        md: `text-base h-9`,
+      },
+
+      variant: {
+        gray: `
+        bg-gray-500
+        hover:bg-gray-600
+      `,
+        primary: `
+        text-white bg-red
+        hover:bg-purple-600
+      `,
+      },
+      outlined: {
+        true: `bg-transparent ring-1`,
+      },
+    },
+
+    defaults: {
+      variant: "gray",
+      size: "sm",
+    },
+
+    matches: [
+      {
+        variant: "gray",
+        outlined: true,
+        use: `ring-gray-500`,
+      },
+      {
+        variant: "primary",
+        outlined: true,
+        use: `text-purple-500 ring-gray-500 hover:text-white`,
+      },
+    ],
+  });
+  console.log("dfd");
+
+  // "...style" enables switching between auto & fixed sizing
+  // Learn more: https://www.framer.com/docs/guides/auto-sizing
+  return (
+    <>
+      <h1 className={tw`font-bold text(center 5xl white sm:gray-800 md:red)`}>
+        This is Twind!
+      </h1>
+      <Button variant="primary">Primary Button</Button>
+    </>
+  );
+};
+
+Test2.defaultProps = {
+  text: "Tap",
+};
+
+addPropertyControls(Test2, {
+  title: {
+    title: "lol",
+    type: ControlType.String,
+    defaultValue: "Title",
+  },
+});
+
+// Learn More: https://www.framer.com/docs/property-controls/
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  overflow: "hidden",
+};
+
+const squareStyle = {
+  margin: 50,
+  padding: 50,
+  color: "white",
+  fontWeight: 600,
+  borderRadius: 25,
+  backgroundColor: "#09F",
+  width: "max-content",
+  whiteSpace: "pre-wrap" as const,
+  flexShrink: 0,
+};
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+export { Test2 } from "./Button";
+export { Battery } from "./Battery";
+export { MotionButton } from "./MotionButton";
