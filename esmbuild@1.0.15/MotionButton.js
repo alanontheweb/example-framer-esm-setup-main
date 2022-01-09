2f1059409ccb6669c5c8721e4c7cf3bf1cb6c403
deploy <deploy@users.noreply.github.com>
2022-01-09 03:02:15 +0000

    Updated

--- esmbuild@1.0.15/MotionButton.js
+++ esmbuild@1.0.15/MotionButton.js
@@ -1,12 +1,13 @@
+// src/MotionButton.tsx
 import React from "react";
 import { motion } from "framer-motion";
-const style = {
+var style = {
   backgroundColor: "red",
   display: "inline-block",
   width: 100,
   height: 100
 };
-const MotionButton = () => /* @__PURE__ */ React.createElement(motion.button, {
+var MotionButton = () => /* @__PURE__ */ React.createElement(motion.button, {
   whileHover: { scale: 0.9 },
   whileTap: { scale: 0.7 },
   style
