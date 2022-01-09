b6e0021e6f2a7c3ee35368314fb3a8a98da0a019
deploy <deploy@users.noreply.github.com>
2022-01-09 08:11:50 +0000

    Updated

---
+++ esmbuild@1.0.23/index.js
@@ -0,0 +1,9 @@
+// src/index.tsx
+import { Test2 } from "./Button.js";
+import { Setup } from "./Setup.js";
+import { MotionButton } from "./MotionButton.js";
+export {
+  MotionButton,
+  Setup,
+  Test2
+};
