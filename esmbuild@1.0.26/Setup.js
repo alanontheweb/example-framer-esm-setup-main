df87e8f25218da3a166451d84ddb84522f1e518e
deploy <deploy@users.noreply.github.com>
2022-01-09 18:20:21 +0000

    Updated

---
+++ esmbuild@1.0.26/Setup.js
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    gray: "red",
+    red: "green",
+    blue: "green",
+    yellow: "yellow"
+  },
+  fontFamily: {
+    sans: ["Helvetica", "sans-serif"],
+    serif: ["Times", "serif"]
+  },
+  extend: {
+    spacing: {
+      128: "32rem",
+      144: "36rem"
+    }
+  }
+};
+export {
+  theme
+};
