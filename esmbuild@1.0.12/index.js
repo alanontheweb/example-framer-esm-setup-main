a5d9f2571d75b9d8aae0c84ed660cad89098c3a0
deploy <deploy@users.noreply.github.com>
2022-01-09 02:44:10 +0000

    Updated

---
+++ esmbuild@1.0.12/index.js
@@ -0,0 +1,9 @@
+// src/index.tsx
+import { Test2 } from "./Button.js";
+import { Battery } from "./Battery.js";
+import { MotionButton } from "./MotionButton.js";
+export {
+  Battery,
+  MotionButton,
+  Test2
+};
