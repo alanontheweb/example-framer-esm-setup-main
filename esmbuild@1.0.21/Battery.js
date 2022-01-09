b24b26fd29f811c088406db24ea0dcd60e949645
deploy <deploy@users.noreply.github.com>
2022-01-09 05:56:59 +0000

    Updated

---
+++ esmbuild@1.0.21/Battery.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useBattery } from "react-use";
+const Battery = () => {
+  const batteryState = useBattery();
+  if (!batteryState.isSupported) {
+    return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("strong", null, "Battery sensor"), ": ", /* @__PURE__ */ React.createElement("span", null, "not supported"));
+  }
+  if (!batteryState.fetched) {
+    return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("strong", null, "Battery sensor"), ": ", /* @__PURE__ */ React.createElement("span", null, "supported"), " ", /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("strong", null, "Battery state"), ": ", /* @__PURE__ */ React.createElement("span", null, "fetching"));
+  }
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("strong", null, "Battery sensor"), ":\xA0\xA0 ", /* @__PURE__ */ React.createElement("span", null, "supported"), " ", /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("strong", null, "Battery state"), ": ", /* @__PURE__ */ React.createElement("span", null, "fetched"), " ", /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("strong", null, "Charge level"), ":\xA0\xA0", " ", /* @__PURE__ */ React.createElement("span", null, (batteryState.level * 100).toFixed(0), "%"), " ", /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("strong", null, "Charging"), ":\xA0\xA0", " ", /* @__PURE__ */ React.createElement("span", null, batteryState.charging ? "yes" : "no"), " ", /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("strong", null, "Charging time"), ":\xA0\xA0", /* @__PURE__ */ React.createElement("span", null, batteryState.chargingTime ? batteryState.chargingTime : "finished"), " ", /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement("strong", null, "Discharging time"), ":\xA0\xA0", " ", /* @__PURE__ */ React.createElement("span", null, batteryState.dischargingTime));
+};
+export {
+  Battery
+};
